7cab21d8df74036ce53f7ee333e1b9de328d2eac
lpasa <lpasa@math-unipd-mAir.local>
2021-08-24 15:28:06 +0200

    update code

--- experiments/citeseer/SMGGNN_shallow.py
+++ experiments/citeseer/SMGGNN_shallow.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 import torch
 from torch.nn import Softmax, ReLU
 from dataReader.dataReader import DGLDatasetReader
